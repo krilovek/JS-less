cf8c420c85bbf8eb385c75714680286f2461cd12
krilovek <75037694+krilovek@users.noreply.github.com>
2022-02-03 21:00:05 +0300

    Add start
    add source file

---
+++ JS start/JS/script.js
@@ -0,0 +1,7 @@
+const textElement = document.querySelector(".content");
+console.log(textElement)
+
+const textElementContent = textElement.innerHTML;
+console.log(textElementContent)
+
+textElement.innerHTML = "Живи. А работай в свободное время"
